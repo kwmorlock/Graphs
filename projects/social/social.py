1798f4aaca6cfb44af5efa38dd72f25561251cc3
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-07-15 16:04:16 -0400

    comments added to social

--- projects/social/social.py
+++ projects/social/social.py
@@ -46,20 +46,26 @@ class SocialGraph:
         self.friendships = {}
         # !!!! IMPLEMENT ME
 
+        # Add users
         for i in range(num_users):
             self.add_user(f"User{i}")
 
+        # Create friendships
         possible_friendships = []
+
         for user_id in self.users:
             for friend_id in range(user_id + 1, self.last_id +1):
                 possible_friendships.append((user_id, friend_id))
 
+        # Shuffle the possible friendships
         random.shuffle(possible_friendships)
 
+       
+        # Add friendships
         for i in range(num_users * avg_friendships // 2):
             friendship = possible_friendships[i]
 
-            self.add_friendship(friendship[0], friendship[i])
+            self.add_friendship(friendship[0], friendship[1])
 
         # Add users
 
